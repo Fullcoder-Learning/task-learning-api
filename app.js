dfb8446a19ad238ba2c2b7f86569ada0ba4f9f64
Guillermo Granados Gómez <pytonicus@gmail.com>
2022-09-01 14:08:39 +0200

    Phase 4 complete

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const app = express();
 
-// importar cors:
 const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
-// habilitar todas las peticiones cors:
 app.use(cors());
 
 const taskRoutes = require('./routes/taskRoutes');
-// cargar modulo rutas de usuarios:
 const userRoutes = require('./routes/userRoutes');
 
 app.use("/api", taskRoutes);
-// cargar rutas de usuarios:
 app.use("/api", userRoutes);
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,15 @@
 const bcryptjs = require('bcryptjs');
 const nodemailer = require('nodemailer');
-// importar momentjs:
 const moment = require('moment');
+const path = require('path');
+const fs = require('fs');
+// importar has de underscore para comprobar que existe una clave en un objeto:
+const {has} = require('underscore');
+const {_} = require('underscore');
+
 const User = require('../models/userModel');
 const jwt = require('../services/jwtService');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 async function postUser(req, res){
     const params = req.body;
@@ -82,7 +88,7 @@ function forgot(req, res){
                     from: 'pytonicus@gmail.com',
                     to: userData.email,
                     subject: 'Reestablecer contraseña | Task Learn',
-                    text: `http://localhost:3000/api/reset/${userData._id}/${token}`
+                    text: `http://localhost:3000/api/reset/${userData.id}/${token}`
                 };
 
                 transporter.sendMail(mailOptions, (err, response)=>{
@@ -100,28 +106,24 @@ function forgot(req, res){
 }
 
 async function resetPassword(req, res){
-    // recuperar el id y token de usuario:
     const {id, token} = req.params;
-    // recuperamos la contraseña por partida doble:
     const {newPassword, repitePassword} = req.body;
 
-    // decodificar token:
     const user_token = jwt.decodeToken(token, process.env.SECRET_KEY);
 
 
-    // comprobar si las contraseñas son diferentes o el token ha caducado o el id son distintos:
     if(user_token.exp <= moment().unix()){
         res.status(400).send({msg: "Error: token has expired"});
     }else if(user_token.id !== id || newPassword !== repitePassword){
         res.status(403).send({msg: "Error: unauthorized request"});
     }else{
-        // codificar contraseña:
         const salt = bcryptjs.genSaltSync(10);
         const password = await bcryptjs.hash(newPassword, salt);
-        // actualizar contraseña nueva:
         User.findByIdAndUpdate(id, {password: password}, (err, result) =>{
             if(err){
                 res.status(500).send({msg: "Server status error"});
+            }else if(!result){
+                res.status(404).send({msg: "Error: user doesn't exists"});
             }else{
                 res.status(200).send({msg: "Password change successfully"});
             }
@@ -130,9 +132,152 @@ async function resetPassword(req, res){
 
 }
 
+async function getUser(req, res){
+    const userId = req.params.id;
+    const user_token = await authMiddleware.getUser(req, res);
+
+    try{
+        const user = await User.findById(userId);
+
+        if(!user){
+            res.status(400).send({msg: "Error: user doesn't exists"});
+        }else if(user.id != user_token.id){ 
+            res.status(403).send({msg: "Forbidden - Access to this resource on the server is denied!"});
+        }else{
+            user.password = null;
+            res.status(200).send({user: user});
+        }
+    }catch(error){
+        res.status(500).send(error);
+    }
+}
+
+async function putUser(req, res){
+    const userId = req.params.id;
+    const params = req.body;
+    
+    const user_token = await authMiddleware.getUser(req, res);
+
+    try{
+        User.findById(userId, async (err, userData) => {
+            if(err){
+                res.status(500).send({msg: "Server status error"});
+            }else{
+                if(!userData){
+                    res.status(404).send({msg: "Error: user doesn't exists"});
+                }else if(user_token.id !== userData.id){
+                    res.status(403).send({msg: "Error: unauthorized request"});
+                }else{
+                    const salt = bcryptjs.genSaltSync(10);
+                    userData.name = params.name;
+                    userData.lastname = params.lastname;
+                    userData.email = params.email; 
+                    userData.password = await bcryptjs.hash(params.password, salt);
+
+                    if(req.files.avatar){
+                        const filePath = req.files.avatar.path;
+                        let fileSplit = path.resolve(filePath).split(path.sep);
+                        let filename = fileSplit[fileSplit.length-1];
+
+                        let fileExt = filename.split(".");
+                        if(fileExt[fileExt.length-1] !== "jpg" && fileExt[fileExt.length-1] !== "jpeg" && fileExt[fileExt.length-1] !== "png"){
+                            fs.unlink(filePath, (err)=>{
+                                if (err) throw console.error(err);
+                                console.error("Only extension can be JPG or PNG");
+                            });
+                        }else{
+                            if(userData.avatar){
+                                const old_path = `./uploads/avatars/${userData.avatar}`;
+                                fs.unlink(old_path, (err)=>{
+                                    if (err) throw console.error(err);
+                                });
+                            }
+                            userData.avatar = filename;
+                        }
+                    }
+
+                    User.findByIdAndUpdate(userId, userData, (err, result) => {
+                        if(err){
+                            res.status(500).send({msg: "Server status error"});
+                        }else if(!result){
+                            res.status(404).send({msg: "Error: user doesn't exists"});
+                        }else{
+                            res.status(201).send({user: userData});
+                        }
+                    });
+
+                }
+            }
+        });
+    }catch(error){
+        console.error(error);
+    }
+}
+
+function getAvatar(req, res){
+    const avatarName = req.params.avatarName;
+    const filePath = `./uploads/avatars/${avatarName}`;
+
+    fs.stat(filePath, (err, stat)=>{
+        if(err){
+            res.status(404).send({msg: "Error: Avatar doesn't exists"});
+        }else{
+            res.sendFile(path.resolve(filePath));
+        }
+    });
+}
+
+// crear función para eliminar usuarios:
+async function deleteUser(req, res){
+    // coger el parametro id:
+    const user_token = await authMiddleware.getUser(req, res);
+    const userId = req.params.id;
+
+    try{
+        // recuperar usuario a eliminar:
+        User.findById(userId, (err, userData)=>{
+            if(err){
+                res.status(500).send({msg: "Server status error"});
+            }else if(user_token.id !== userData.id){
+                res.status(403).send({msg: "Error: unauthorized request"});
+            }else{
+                // evitar errores al cargar nombre de avatar:
+                try{
+                    var avatar = userData.avatar;
+                }catch{
+                    var avatar = null;
+                }
+
+                User.findByIdAndDelete(userId, (err, result)=>{
+                    if(err){
+                        res.status(500).send({msg: "Server status error"});
+                    }else if(!result){
+                        res.status(404).send({msg: "Error: User doesn't exist"});
+                    }else{
+                        // si existe un avatar lo eliminamos:
+                        if(avatar){
+                            const old_path = `./uploads/avatars/${avatar}`;
+                            fs.unlink(old_path, (err)=>{
+                                if (err) throw console.error(err);
+                            });
+                        }
+                        res.status(200).send({msg: "User delete successfully"});
+                    }
+                });
+            }
+        });
+    }catch(error){
+        res.status(500).send({msg: "Server status error"});
+    }
+}
+
 module.exports = {
     postUser,
     login,
     forgot,
-    resetPassword // exportar modulo
+    resetPassword,
+    getUser,
+    putUser,
+    getAvatar,
+    deleteUser // exportar modulo
 }
